79118cecebcd024152ca27cf27042dd80b7ab7e9
farhan <blackichan200@gmail.com>
2019-05-21 11:10:21 +0100

    first commit

---
+++ src/main/java/com/Qa/account_application/App.java
@@ -0,0 +1,13 @@
+package com.Qa.account_application;
+
+public class App 
+{
+	
+    public static void main( String[] args )
+    
+    {
+    	
+        System.out.println( "Hello World to the standard out" );
+        
+    }
+}
